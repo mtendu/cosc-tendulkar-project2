6d4483674c96937f2ea68c659c400f325a6d30d8
Madhura Tendulkar <madhuratendulkar09@gmail.com>
2016-03-02 13:55:09 -0600

    Extracted methods: getDiceValue() from applyAction() in MovePlayerCard class colorGroups() from exchangeProperty() in Player class

--- Monopoly/Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/MovePlayerCard.java
+++ Monopoly/Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/MovePlayerCard.java
@@ -28,21 +28,31 @@ public class MovePlayerCard extends Card {
      * @see edu.towson.cis.cosc442.project1.monopoly.Card#applyAction()
      */
     public void applyAction() {
-        Player currentPlayer = GameMaster.instance().getCurrentPlayer();
-        Cell currentPosition = currentPlayer.getPosition();
-        int newCell = GameMaster.instance().getGameBoard().queryCellIndex(destination);
-        int currentCell = GameMaster.instance().getGameBoard().queryCellIndex(currentPosition.getName());
-        int diceValue = 0;
-        if(currentCell > newCell) {
-            diceValue = (GameMaster.instance().getGameBoard().getCellNumber() + 
-                    (newCell - currentCell));
-        }
-        else if(currentCell <= newCell) {
-            diceValue = newCell - currentCell;
-        }
-        System.out.println(diceValue);
-        GameMaster.instance().movePlayer(currentPlayer, diceValue);
+        int diceValue = getDiceValue();
+		System.out.println(diceValue);
     }
+/**
+ * extracted getDiceValue() from applyAction().
+ * @return
+ */
+	private int getDiceValue() {
+		Player currentPlayer = GameMaster.instance().getCurrentPlayer();
+		Cell currentPosition = currentPlayer.getPosition();
+		int newCell = GameMaster.instance().getGameBoard()
+				.queryCellIndex(destination);
+		int currentCell = GameMaster.instance().getGameBoard()
+				.queryCellIndex(currentPosition.getName());
+		int diceValue = 0;
+		if (currentCell > newCell) {
+			diceValue = (GameMaster.instance().getGameBoard().getCellNumber() + (newCell - currentCell));
+		} else {
+			if (currentCell <= newCell) {
+				diceValue = newCell - currentCell;
+			}
+		}
+		GameMaster.instance().movePlayer(currentPlayer, diceValue);
+		return diceValue;
+	}
 
     /* (non-Javadoc)
      * @see edu.towson.cis.cosc442.project1.monopoly.Card#getCardType()
